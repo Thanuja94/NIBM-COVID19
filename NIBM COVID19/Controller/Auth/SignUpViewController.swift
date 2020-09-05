10963c47c5a1f9786a1d1bf429f6866b64939d2a
Thanuja94 <thanujag94@gmail.com>
2020-09-06 03:08:56 +0530

    add signup view controller

---
+++ NIBM COVID19/Controller/Auth/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/6/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    // MARK: - Properties
+    
+//    private let imageview: UIImageView(){
+//
+//    }
+    
+    // MARK: - LifeCycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   // MARK: - Fuctions
+}
