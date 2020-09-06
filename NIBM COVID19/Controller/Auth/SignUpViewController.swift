0ed1a3b3fe0da3d1979702e7845fe736503860fc
Thanuja94 <thanujag94@gmail.com>
2020-09-06 15:45:34 +0530

    add signup label, already have button, set the navigate
    add the above components and set the navigation to login view controller and create acc view controller

--- NIBM COVID19/Controller/Auth/SignUpViewController.swift
+++ NIBM COVID19/Controller/Auth/SignUpViewController.swift
@@ -23,7 +23,32 @@ class SignUpViewController: UIViewController {
         button.layer.cornerRadius = 10
         button.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
+         button.addTarget(self, action: #selector(ShowCreateAcc), for: .touchUpInside)
+        
         return button
+    }()
+    
+    let alreadyHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Already have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: colors.aquavelvet])
+        
+        button.addTarget(self, action: #selector(loginview), for: .touchUpInside)
+       
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    private let BysigninLabel: UILabel = {
+        let label = UILabel()
+        label.text = "By signin up, you agree with the terms of services and privacy policy"
+        label.font = UIFont(name: "Avenir-Light" , size: 15)
+        label.textColor = .gray//UIColor(white: 1, alpha: 0.8)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+       
+        
+        return label
     }()
     
     // MARK: - LifeCycles
@@ -37,7 +62,7 @@ class SignUpViewController: UIViewController {
         imageviewVirus.anchor(top: view.safeAreaLayoutGuide.topAnchor ,paddingTop: 200)
         imageviewVirus.centerX(inView: view)
         
-        let stack = UIStackView(arrangedSubviews: [SignUpButton])
+        let stack = UIStackView(arrangedSubviews: [SignUpButton,alreadyHaveAccountButton])
         stack.axis = .vertical
         stack.distribution = .fillEqually
         stack.spacing = 16
@@ -45,10 +70,24 @@ class SignUpViewController: UIViewController {
         view.addSubview(stack)
         stack.anchor(top: imageviewVirus.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 60, paddingLeft: 16, paddingRight: 16)
         
+        view.addSubview(BysigninLabel)
+        BysigninLabel.anchor( left: view.leftAnchor,   bottom: view.safeAreaLayoutGuide.bottomAnchor, right:view.rightAnchor,paddingLeft: 20, paddingBottom: 50 ,paddingRight: 20)
+        BysigninLabel.centerX(inView: view)
+        
         navigationController?.navigationBar.isHidden = true
         
     }
     
     
     // MARK: - Fuctions
+    
+    @objc func loginview() {
+           let loginviewcontroller = LoginViewController()
+           navigationController?.pushViewController(loginviewcontroller, animated: true)
+       }
+
+    @objc func ShowCreateAcc() {
+         let createaccviewcontroller = CreateAccViewController()
+         navigationController?.pushViewController(createaccviewcontroller, animated: true)
+     }
 }
--- NIBM COVID19/Utils/AppColors.swift
+++ NIBM COVID19/Utils/AppColors.swift
@@ -12,5 +12,8 @@ struct colors {
     static let cynaite = UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1)
     static let silver = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
     static let midnightblue = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1)
+    static let aquavelvet = UIColor(red: 1/255, green: 163/255, blue: 164/255, alpha: 1)
+    
+
    
 }
