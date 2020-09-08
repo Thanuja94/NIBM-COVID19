06ab4605fa21223ee3bbcee53b02b5b28dda720c
Thanuja94 <thanujag94@gmail.com>
2020-09-08 23:15:27 +0530

    add survayq1 view controller

---
+++ NIBM COVID19/Controller/Survay/SurvayQ1ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SurvayQ1ViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/8/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class SurvayQ1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
