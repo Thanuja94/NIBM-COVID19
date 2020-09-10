35593b02e9ca53943e2ec70d2b9bb284fafb489e
Thanuja94 <thanujag94@gmail.com>
2020-09-10 12:56:23 +0530

    add lable, button tothe survayq4

--- NIBM COVID19/Controller/Survay/SurvayQ4ViewController.swift
+++ NIBM COVID19/Controller/Survay/SurvayQ4ViewController.swift
@@ -10,21 +10,73 @@ import UIKit
 
 class SurvayQ4ViewController: UIViewController {
 
+    // MARK: - Properties
+       
+       let survayq4image = UIImageView(image: #imageLiteral(resourceName: "survayq3"))
+       
+       let YesButton: UIButton = {
+                 let button = UIButton(type: .system)
+                 let attributedTitle = NSMutableAttributedString(string: "Yes", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24), NSAttributedString.Key.foregroundColor: colors.aquavelvet])
+                 
+                 //button.addTarget(self, action: #selector(loginview), for: .touchUpInside)
+                
+                 
+                 button.setAttributedTitle(attributedTitle, for: .normal)
+                 return button
+             }()
+       
+       let NoButton: UIButton = {
+                    let button = UIButton(type: .system)
+                    let attributedTitle = NSMutableAttributedString(string: "No", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24), NSAttributedString.Key.foregroundColor: colors.aquavelvet])
+                    
+                    //button.addTarget(self, action: #selector(loginview), for: .touchUpInside)
+                   
+                    
+                    button.setAttributedTitle(attributedTitle, for: .normal)
+                    return button
+                }()
+       
+       private let Question: UILabel = {
+             let label = UILabel()
+             label.text = "Have you been been interact with any sick person recently?"
+             label.font = UIFont(name: "Avenir-Light" , size: 25)
+             label.textColor = .black//UIColor(white: 1, alpha: 0.8)
+             label.numberOfLines = 0
+             label.textAlignment = .center
+            
+             
+             return label
+         }()
+       // MARK: - Lifecycale
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUi()
+    }
+    
+    func setupUi(){
+        view.backgroundColor = colors.silver
+        
+        view.addSubview(YesButton)
+        YesButton.anchor( left: view.leftAnchor,   bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingLeft: 40, paddingBottom: 50 )
+        //YesButton.centerX(inView: view)
+        
+        view.addSubview(NoButton)
+        NoButton.anchor( bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,  paddingBottom: 50, paddingRight: 40)
+        
+        view.addSubview(survayq4image)
+        survayq4image.anchor(top: view.safeAreaLayoutGuide.topAnchor ,paddingTop: 100)
+        survayq4image.centerX(inView: view)
+        
+        view.addSubview(Question)
+        Question.anchor(top: survayq4image.bottomAnchor , left: view.leftAnchor ,right: view.rightAnchor , paddingTop: 20, paddingLeft:20 , paddingRight: 20 )
+        Question.centerX(inView: view)
+        
+      navigationController?.navigationBar.isHidden = true
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
