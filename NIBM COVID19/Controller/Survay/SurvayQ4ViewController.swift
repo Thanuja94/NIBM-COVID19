d9e2c72b6bc75391b619f1d68d12d9f5cd7cfd59
Thanuja94 <thanujag94@gmail.com>
2020-09-10 12:48:08 +0530

    add survayq4 view controller

---
+++ NIBM COVID19/Controller/Survay/SurvayQ4ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SurvayQ4ViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/10/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class SurvayQ4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
