2e579abb4624e130b618a0c2274c697bbd12c225
Thanuja94 <thanujag94@gmail.com>
2020-09-13 03:04:24 +0530

    add about us lable

--- NIBM COVID19/Controller/Settings/AboutUsViewController.swift
+++ NIBM COVID19/Controller/Settings/AboutUsViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class AboutUsViewController: UIViewController {
     
-       // MARK: - Properties
-      let titleLableView = UIView()
+    // MARK: - Properties
+    let titleLableView = UIView()
+    
+    private let aboutUsLable: UILabel = {
+        let label = UILabel()
+        label.text = "Contact Us/ About Us"
+        label.font = UIFont(name: "Avenir-Light" , size: 25)
+        label.textColor = .black
+        
+        return label
+    }()
 
+ // MARK: - Lifecycale
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
       
     }
     
+    // MARK: - Functions
 
+    func setupUI()  {
+        view.backgroundColor = .brown
+    }
     
 }
