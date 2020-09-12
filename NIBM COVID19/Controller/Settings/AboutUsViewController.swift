763fd6b7d7ddfbfa73c609bf7b552c0e6e2d0bc2
Thanuja94 <thanujag94@gmail.com>
2020-09-13 02:58:09 +0530

    add about us view controller

---
+++ NIBM COVID19/Controller/Settings/AboutUsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutUsViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/13/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+    
+       // MARK: - Properties
+      let titleLableView = UIView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    
+}
