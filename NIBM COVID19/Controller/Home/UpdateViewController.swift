1af75b30e93fd39d74c3cc455a6dff3093465147
Thanuja94 <thanujag94@gmail.com>
2020-09-12 18:30:52 +0530

    create update view controller

---
+++ NIBM COVID19/Controller/Home/UpdateViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UpdateViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/12/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class UpdateViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycale
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+       
+    }
+    
+    // MARK: - Functions
+  
+
+}
