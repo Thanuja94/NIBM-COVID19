722070ea8a24a8a05623ed6b5eb9dd24458fea2d
Thanuja94 <thanujag94@gmail.com>
2020-09-13 03:31:21 +0530

    add profile view controller

---
+++ NIBM COVID19/Controller/Settings/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/13/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    // MARK: - Properties
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+}
