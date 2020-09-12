52ae670eaf9a0d073a3b3238ef2ca249cd8f6bc7
Thanuja94 <thanujag94@gmail.com>
2020-09-13 00:21:22 +0530

    create settings view controller

---
+++ NIBM COVID19/Controller/Settings/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/13/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
