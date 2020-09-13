649bb21eaf0865f7914c089761b6bb9dc7b1916a
Thanuja94 <thanujag94@gmail.com>
2020-09-13 14:48:28 +0530

    add table view to notification screen

--- NIBM COVID19/Controller/Notifications/NotificationViewController.swift
+++ NIBM COVID19/Controller/Notifications/NotificationViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
     
     // MARK: - Properties
        let tableView = UITableView()
 
+    // MARK: - Lifecycale
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTableView()
 
        
     }
     
+        // MARK: - Functions
+    
+    func setTableView()  {
+        tableView.frame = self.view.frame
+        tableView.backgroundColor = UIColor.clear
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorColor = UIColor.clear
+        self.view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+    }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
+    }
+    
 }
+ 
