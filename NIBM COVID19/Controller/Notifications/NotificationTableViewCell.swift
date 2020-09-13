d7ad12a977c7fc7356e90a17d0fcfc794447e80d
Thanuja94 <thanujag94@gmail.com>
2020-09-13 14:57:54 +0530

    add notification table cell controller

---
+++ NIBM COVID19/Controller/Notifications/NotificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationTableViewCell.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/13/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
