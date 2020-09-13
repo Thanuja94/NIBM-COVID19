d2223f9b4bf2bc6152e2ae7fe5897573f88b0913
Thanuja94 <thanujag94@gmail.com>
2020-09-13 19:59:26 +0530

    create map view controller

---
+++ NIBM COVID19/Controller/Home/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/13/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+}
