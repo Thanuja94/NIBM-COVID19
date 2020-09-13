69f447ea14586037c223e8fc0aefe021b7197891
Thanuja94 <thanujag94@gmail.com>
2020-09-13 12:21:53 +0530

    create notification view controller

---
+++ NIBM COVID19/Controller/Notifications/NotificationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationViewController.swift
+//  NIBM COVID19
+//
+//  Created by Thanuja Nayanajith on 9/13/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+    
+    // MARK: - Properties
+       let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
