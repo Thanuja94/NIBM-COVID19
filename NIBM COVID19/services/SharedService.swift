1cfdf03087966abcd82d379275406c36c4995d96
Thanuja94 <thanujag94@gmail.com>
2020-09-14 12:10:23 +0530

    create user model and fetchuserdata , location functions in service

--- NIBM COVID19/services/SharedService.swift
+++ NIBM COVID19/services/SharedService.swift
@@ -18,28 +18,28 @@ struct Service {
     
     static let shared = Service()
     
-//    func fetchUserData(uid: String, completion: @escaping(User) -> Void) {
-//        REF_USERS.child(uid).observeSingleEvent(of: .value) { (snapshot) in
-//            guard let dictionary = snapshot.value as? [String: Any] else { return }
-//            let uid = snapshot.key
-//            let user = User(uid: uid, dictionary: dictionary)
-//            completion(user)
-//        }
-//    }
-//
-//    func fetchUsersLocation(location: CLLocation, completion: @escaping(User) -> Void) {
-//           let geoFire = GeoFire(firebaseRef: REF_USER_LOCATIONS)
-//
-//           REF_USER_LOCATIONS.observe(.value) { (snapshot) in
-//               geoFire.query(at: location, withRadius: 50).observe(.keyEntered, with: { (uid, location) in
-//                   self.fetchUserData(uid: uid) { (user) in
-//                       var driver = user
-//                       driver.location = location
-//                       completion(driver)
-//                   }
-//               })
-//           }
-//       }
+    func fetchUserData(uid: String, completion: @escaping(User) -> Void) {
+        REF_USERS.child(uid).observeSingleEvent(of: .value) { (snapshot) in
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            let uid = snapshot.key
+            let user = User(uid: uid, dictionary: dictionary)
+            completion(user)
+        }
+    }
+
+    func fetchUsersLocation(location: CLLocation, completion: @escaping(User) -> Void) {
+           let geoFire = GeoFire(firebaseRef: REF_USER_LOCATIONS)
+
+           REF_USER_LOCATIONS.observe(.value) { (snapshot) in
+               geoFire.query(at: location, withRadius: 50).observe(.keyEntered, with: { (uid, location) in
+                   self.fetchUserData(uid: uid) { (user) in
+                       var driver = user
+                       driver.location = location
+                       completion(driver)
+                   }
+               })
+           }
+       }
 }
 
 
