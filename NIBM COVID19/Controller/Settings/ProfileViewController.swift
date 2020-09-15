d0b2c61738954a581972b931cf0029451cf593b6
Thanuja94 <thanujag94@gmail.com>
2020-09-15 14:11:39 +0530

    add name textfield, index textfield with palceholders

--- NIBM COVID19/Controller/Settings/ProfileViewController.swift
+++ NIBM COVID19/Controller/Settings/ProfileViewController.swift
@@ -36,6 +36,37 @@ class ProfileViewController: UIViewController {
         button.setAttributedTitle(attributedTitle, for: .normal)
         return button
     }()
+    
+    private let nameLable: UILabel = {
+        let label = UILabel()
+        label.text = "James Smith"
+        label.font = UIFont(name: "Avenir-Light" , size: 25)
+        label.textColor = .black
+        
+        return label
+    }()
+
+    private let nameTextFiled: UITextField = {
+           let textfield = UITextField()
+           textfield.borderStyle = .line
+           textfield.textColor = .black
+        textfield.placeholder = "Name"
+           
+           
+           return textfield
+           
+       }()
+    
+    private let indexTextFiled: UITextField = {
+              let textfield = UITextField()
+              textfield.borderStyle = .line
+              textfield.textColor = .black
+           textfield.placeholder = "Index"
+              
+              
+              return textfield
+              
+          }()
 
 
     // MARK: - LifeCycles
@@ -54,7 +85,6 @@ class ProfileViewController: UIViewController {
         
         view.backgroundColor = .white
         
-        profileNameView.backgroundColor = .red
         view.addSubview(profileNameView)
         profileNameView.translatesAutoresizingMaskIntoConstraints = false
         profileNameView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 10).isActive = true
@@ -78,7 +108,7 @@ class ProfileViewController: UIViewController {
         profileDetailView.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor, multiplier: 0.15).isActive = true
         profileDetailView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -5).isActive = true
         
-        profileNameTextView.backgroundColor = .orange
+       
         view.addSubview(profileNameTextView)
         profileNameTextView.translatesAutoresizingMaskIntoConstraints = false
         profileNameTextView.topAnchor.constraint(equalTo: profileDetailView.bottomAnchor, constant: 10).isActive = true
@@ -86,7 +116,7 @@ class ProfileViewController: UIViewController {
         profileNameTextView.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor, multiplier: 0.1).isActive = true
         profileNameTextView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -5).isActive = true
         
-        indexTextView.backgroundColor = .brown
+        
                view.addSubview(indexTextView)
                indexTextView.translatesAutoresizingMaskIntoConstraints = false
                indexTextView.topAnchor.constraint(equalTo: profileNameTextView.bottomAnchor, constant: 5).isActive = true
@@ -116,6 +146,29 @@ class ProfileViewController: UIViewController {
         updateButton.topAnchor.constraint(equalTo: updateButtonView.topAnchor, constant: 10).isActive = true
         updateButton.centerXAnchor.constraint(equalTo: updateButtonView.centerXAnchor).isActive = true
 
+        profileNameView.addSubview(nameLable)
+        nameLable.translatesAutoresizingMaskIntoConstraints = false
+        nameLable.heightAnchor.constraint(equalTo: profileNameView.heightAnchor, multiplier: 0.5).isActive = true
+        nameLable.topAnchor.constraint(equalTo: profileNameView.topAnchor, constant: 10).isActive = true
+        nameLable.centerXAnchor.constraint(equalTo: profileNameView.centerXAnchor).isActive = true
+        nameLable.centerYAnchor.constraint(equalTo: profileNameView.centerYAnchor).isActive = true
+        
+        
+        profileNameTextView.addSubview(nameTextFiled)
+               nameTextFiled.translatesAutoresizingMaskIntoConstraints = false
+               nameTextFiled.heightAnchor.constraint(equalTo: profileNameTextView.heightAnchor, multiplier: 0.8).isActive = true
+               nameTextFiled.topAnchor.constraint(equalTo: profileNameTextView.topAnchor, constant: 10).isActive = true
+        nameTextFiled.trailingAnchor.constraint(equalTo: profileNameTextView.trailingAnchor, constant: -10).isActive = true
+        nameTextFiled.leadingAnchor.constraint(equalTo: profileNameTextView.leadingAnchor, constant: 10).isActive = true
+
+        indexTextView.addSubview(indexTextFiled)
+                      indexTextFiled.translatesAutoresizingMaskIntoConstraints = false
+                      indexTextFiled.heightAnchor.constraint(equalTo: indexTextView.heightAnchor, multiplier: 0.8).isActive = true
+                      indexTextFiled.topAnchor.constraint(equalTo: indexTextView.topAnchor, constant: 10).isActive = true
+               indexTextFiled.trailingAnchor.constraint(equalTo: indexTextView.trailingAnchor, constant: -10).isActive = true
+               indexTextFiled.leadingAnchor.constraint(equalTo: indexTextView.leadingAnchor, constant: 10).isActive = true
+
+        
         
         navigationController?.navigationBar.isHidden = false
         
